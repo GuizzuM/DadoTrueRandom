5028466990ee3f0aadcf56879a0c53cb51a3eaeb
Guizzu <gui.muzzi.gm.gm@gmail.com>
2024-11-13 03:48:45 -0400

    Somar dados iguais por listas encadeadas

--- main.c
+++ main.c
@@ -3,32 +3,32 @@
 #include <windows.h>
 #include <wincrypt.h>
 
-void gerar(int a){
+int gerar(int a){
 
     HCRYPTPROV hProv;
-    BYTE randomBytes[4]; // Array de 4 bytes para gerar um número de 32 bits
+    BYTE randomBytes[4]; // Array de 4 bytes para gerar um nï¿½mero de 32 bits
     unsigned int random_number = 0;
 
-    // Inicializa o provedor de contexto criptográfico
+    // Inicializa o provedor de contexto criptogrï¿½fico
     if (!CryptAcquireContext(&hProv, NULL, NULL, PROV_RSA_FULL, CRYPT_VERIFYCONTEXT)) {
-        printf("Falha ao adquirir o contexto criptográfico\n");
-        return;
+        printf("Falha ao adquirir o contexto criptogrï¿½fico\n");
+        return -1;
     }
 
-    // Gera números aleatórios
+    // Gera nï¿½meros aleatï¿½rios
     if (!CryptGenRandom(hProv, sizeof(randomBytes), randomBytes)) {
-        printf("Falha ao gerar o número aleatório\n");
+        printf("Falha ao gerar o nï¿½mero aleatï¿½rio\n");
         CryptReleaseContext(hProv, 0);
-        return ;
+        return -1;
     }
 
-    // Converte os bytes aleatórios para um número inteiro
+    // Converte os bytes aleatï¿½rios para um nï¿½mero inteiro
     random_number = (randomBytes[0] << 24) | (randomBytes[1] << 16) | (randomBytes[2] << 8) | randomBytes[3];
 
     int max = a;
         a= reduzir(random_number, max);
         imprimir(a);
-
+    return a;
 
 }
 
@@ -40,13 +40,71 @@ int reduzir(unsigned int random_number, int m){
 }
 
 void imprimir(int result){
-    printf("%u ", result);
+    printf("%d ", result);
 }
 
+typedef struct no{//Um dia eu faÃ§o Ã¡rvore BinÃ¡ria ou B sla.
+    int valor;
+    struct no *prox;
+}No;
+typedef struct{
+    No *inicio, *fim;
+ }Lista;
+void inserirInicio(Lista *lista, int valor){
+    No *novo = (No*)malloc(sizeof(No));
+    novo->valor = valor;
+    if(lista->inicio == NULL){
+        novo->prox = NULL;
+        lista->inicio = novo;
+        lista->fim = novo;
+    } else {
+        novo->prox = lista->inicio;
+        lista->inicio = novo;
+    }
+ }
+void somarLista(Lista *lista, int valor){
+    int rep=0;
+    No *inicio = lista->inicio;
+    if(inicio == NULL)
+        return;
+    else{
+        while(inicio != NULL){
+                if(inicio->valor == valor){
+                    rep++;
+                    }
+                inicio = inicio->prox;
+        }
+    }
+    int soma = valor * rep;
+    if (rep == 0 || rep == 1)
+        return;
+    printf("\n\nSoma dos %d's: %d\n", valor, soma);
+}
+
+ void remover(Lista *lista){
+    No *inicio = lista->inicio;
+    No *noARemover = NULL;
+    if(inicio != NULL){
+        while(inicio!= NULL){
+            noARemover = inicio;
+            inicio = inicio->prox;
+            if(noARemover != NULL){
+                free(noARemover);
+            }
+
+        }
+    }
+    lista->inicio = NULL;
+    lista->fim = NULL;
+ }
+
 int main() {
-    HCRYPTPROV hProv;
+    //HCRYPTPROV hProv;
     char entrada[5];
-    int rep;
+    int rep, temp;
+    Lista lista;
+    lista.inicio = NULL;
+    lista.fim = NULL;
 
 do{
         printf("Quantos dados quer jogar?\n00 - Para Sair - cc para limpar\n");
@@ -61,9 +119,13 @@ do{
         { printf("----------------------\n");
             for(int i = 0; i < rep; i++)
                 {
-                gerar(6);
-                Sleep(400);
-                }
+                temp = gerar(6);
+                inserirInicio(&lista, temp);
+                Sleep(350);
+                } for(int i = 1; i < 7; i++){
+                    temp = i;
+                    somarLista(&lista, temp);
+                } remover(&lista);
         printf("\n----------------------\n");
         printf("\n\n");
         } else
@@ -71,6 +133,6 @@ do{
         Sleep(250);
 }while(strcmp(entrada, "00")!= 0);
 
-CryptReleaseContext(hProv, 0);
+//CryptReleaseContext(hProv, 0);
     return 0;
 }
